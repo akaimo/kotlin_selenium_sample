ef00e50cbde3a0656808a9880532fe65a678d30c
shuhei <kawashu0723@gmail.com>
2017-12-06 18:21:23 +0900

    first commit

---
+++ src/main/kotlin/code/example/HelloWorld.kt
@@ -0,0 +1,7 @@
+package code.example
+
+import org.openqa.selenium.chrome.ChromeDriver
+
+fun main(args: Array<String>) {
+    println("Hello World!!_")
+}
---
+++ src/test/kotlin/code/example/HelloWorldTest.kt
@@ -0,0 +1,9 @@
+package code.example
+
+import org.junit.jupiter.api.Test
+
+class HelloWorldTest {
+    @Test fun sample() {
+        println("test----")
+    }
+}
