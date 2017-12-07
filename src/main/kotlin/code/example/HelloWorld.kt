74859d8876d71e789f7fbb0d4214aa92ee219fd9
shuhei <kawashu0723@gmail.com>
2017-12-08 01:01:09 +0900

    add launchar

--- src/main/kotlin/code/example/HelloWorld.kt
+++ src/main/kotlin/code/example/HelloWorld.kt
@@ -1,6 +1,10 @@
 package code.example
 
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder
+import org.junit.platform.engine.discovery.ClassNameFilter.includeClassNamePatterns
 
 fun main(args: Array<String>) {
     println("Hello World!!")
+
+    val request = LauncherDiscoveryRequestBuilder().filters(includeClassNamePatterns(".*Test"))
 }
