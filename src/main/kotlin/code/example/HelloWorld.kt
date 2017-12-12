0791a7d87c53511c39492ec7141b8e44f22290c7
shuhei <kawashu0723@gmail.com>
2017-12-12 14:19:29 +0900

    runからテストが実行できるとこまで

--- src/main/kotlin/code/example/HelloWorld.kt
+++ src/main/kotlin/code/example/HelloWorld.kt
@@ -1,10 +1,45 @@
 package code.example
 
+import org.junit.platform.commons.util.ReflectionUtils
+import org.junit.platform.engine.TestEngine
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder
-import org.junit.platform.engine.discovery.ClassNameFilter.includeClassNamePatterns
+import org.junit.platform.engine.discovery.ClassNameFilter.*
+import org.junit.platform.engine.discovery.DiscoverySelectors
+import org.junit.platform.launcher.EngineFilter.*
+import org.junit.platform.launcher.TagFilter.*
+import org.junit.platform.launcher.TestExecutionListener
+import org.junit.platform.launcher.core.LauncherFactory
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener
+import java.io.PrintWriter
+import java.util.*
 
 fun main(args: Array<String>) {
     println("Hello World!!")
 
-    val request = LauncherDiscoveryRequestBuilder().filters(includeClassNamePatterns(".*Test"))
+    val testEngine = ServiceLoader.load(TestEngine::class.java)
+    val launcher = LauncherFactory.create()
+    val summary = SummaryGeneratingListener()
+    launcher.registerTestExecutionListeners(summary)
+    val request = LauncherDiscoveryRequestBuilder
+            .request()
+//            .selectors(selectPackage("sample.e2e"))
+            .selectors(DiscoverySelectors.selectPackage("sample.e2e"))
+            .filters(includeClassNamePatterns(".*"))
+            .build()
+    launcher.execute(request)
+    summary.summary.printTo(PrintWriter(System.out))
+
+//    val request = LauncherDiscoveryRequestBuilder
+//            .request()
+//            .selectors(selectPackage("sample.e2e"))
+//            .filters(includeClassNamePatterns(".*"))
+//            .build()
+//    val plan = LauncherFactory.create().discover(request)
+//    val launcher = LauncherFactory.create()
+//    val summaryGeneratingListener = SummaryGeneratingListener()
+//    launcher.execute(
+//            request,
+//            summaryGeneratingListener
+//    )
+//    println(summaryGeneratingListener.summary)
 }
